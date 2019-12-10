9f5b587c55aca56d5a10db4946bcc67cc7fbb7a4
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-10 03:34:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const append = ["Milo", "Otis", "Garfield"]
 append.push("Odie")
 const prepend = ["Milo", "Otis", "Garfield"]
-prepend.shift("Odie")
+prepend.unshift("Odie")
 const removeLast = ["Milo", "Otis", "Garfield"]
 const removeFirst = ["Milo", "Otis", "Garfield"]
