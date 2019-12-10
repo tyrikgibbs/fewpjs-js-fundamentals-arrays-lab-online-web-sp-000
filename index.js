96a902932d1b59ffde847a474081870063acc1cc
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-10 03:31:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution here!
 const append = ["Milo", "Otis", "Garfield"]
-append.append("Odie")
+append.push("Odie")
 const prepend = ["Milo", "Otis", "Garfield"]
 const removeLast = ["Milo", "Otis", "Garfield"]
 const removeFirst = ["Milo", "Otis", "Garfield"]
